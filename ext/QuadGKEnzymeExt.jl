e9b07394ff3aa3ae85fba30502e13252e31f785e
Daniel Wennberg <daniel.wennberg@gmail.com>
2025-04-18 07:51:19 -0700

    Another tmp stash commit

--- ext/QuadGKEnzymeExt.jl
+++ ext/QuadGKEnzymeExt.jl
@@ -2,14 +2,16 @@ module QuadGKEnzymeExt
 
 using Enzyme, LinearAlgebra
 using QuadGK: do_quadgk, ReturnSegbuf, InplaceIntegrand
-using .EnzymeRules: RevConfig, needs_primal, needs_shadow, AugmentedReturn, VectorSpace
+using .EnzymeRules: VectorSpace
+
+const runtime_inactive = Val(false)  # XXX: true would be better if it worked and was performant
 
 function EnzymeRules.augmented_primal(
-    config::RevConfig,
+    config::EnzymeRules.RevConfigWidth{1},
     ::Const{typeof(do_quadgk)},
     ::Type{RT},
     f::F,
-    segs::Union{Const{NTuple{N,T}},Active{NTuple{N,T}}},
+    segs::Union{Const{S},Active{S}},
     n::Const,
     atol::Union{Const,Active},  # never actually active, but may be inferred as such by runtime activity
     rtol::Union{Const,Active},  # never actually active, but may be inferred as such by runtime activity
@@ -17,82 +19,109 @@ function EnzymeRules.augmented_primal(
     nrm::Const,
     segbuf::Const,
     eval_segbuf::Const,
-) where {RT<:Union{Active,MixedDuplicated,BatchMixedDuplicated},F,N,T}
+) where {RT<:Union{Const,Active,MixedDuplicated},F,N,T,S<:NTuple{N,T}}
     fv, segsv, othersv..., segbufv, eval_segbufv = map(
         x -> x.val, (f, segs, n, atol, rtol, maxevals, nrm, segbuf, eval_segbuf)
     )
 
-    # whenever we call do_quadgk we hold on to the primal so we can use it for constructing
-    # a shadow if needed, regardless of needs_primal(config)
-    if (segbufv isa ReturnSegbuf) && (!(f isa Const) || needs_primal(config) || needs_shadow(config))
+    needs_primal = EnzymeRules.needs_primal(config)
+    needs_shadow = EnzymeRules.needs_shadow(config)
+    reverse_needs_segbuf = !(f isa Const)
+
+    if (segbufv isa ReturnSegbuf) && (needs_primal || needs_shadow || reverse_needs_segbuf)
         # someone could AD quadgk_segbuf; in this case, we must call do_quadgk if:
         # - !(f isa Const)---we need eval_segbuf_new
         # - needs_primal(config)---as always
         # - needs_shadow(config)---only reliable way to obtain a source for the eval_segbuf part of the shadow
         primal_true = do_quadgk(fv, segsv, othersv..., segbufv, eval_segbufv)
         eval_segbuf_new = last(primal_true)
-    elseif !(f isa Const)  # call do_quadgk because we need eval_segbuf_new
-        @assert !(segbufv isa ReturnSegbuf)  # sanity check, should compile away
+    elseif reverse_needs_segbuf
         primal_true..., eval_segbuf_new = do_quadgk(
             fv, segsv, othersv..., ReturnSegbuf(segbufv), eval_segbufv
         )
-    elseif needs_primal(config)  # call do_quadgk because we need the primal
-        @assert !(segbufv isa ReturnSegbuf)  # sanity check, should compile away
+    elseif needs_primal
         primal_true = do_quadgk(fv, segsv, othersv..., segbufv, eval_segbufv)
         eval_segbuf_new = nothing
-    else  # don't call do_quadgk; no need for either the primal or eval_segbuf_new
+    else  # forward pass not needed
         primal_true, eval_segbuf_new = nothing, nothing
     end
 
-    primal = needs_primal(config) ? primal_true : nothing
+    primal = EnzymeRules.needs_primal(config) ? primal_true : nothing
 
-    shadow = if !needs_shadow(config)
+    shadow = if !needs_shadow
         nothing
     else
         # source such that shadow will comprise one or more copies of make_zero(source)
-        source = if isnothing(primal_true)
+        source = if !isnothing(primal_true)
+            primal_true
+        else  # create mock primal
             a, b = segsv[1], segsv[2]
             Imock = f.val((a + b) / 2) * (b - a)
             Emock = nrm.val(Imock)
             if segbufv isa ReturnSegbuf
-                @assert !isnothing(eval_segbuf_new)  # sanity check, should compile away
+                @assert !isnothing(eval_segbuf_new)
                 (Imock, Emock, eval_segbuf_new)
             else
                 (Imock, Emock)
             end
-        else
-            primal_true
         end
-        W = EnzymeRules.width(config)
-        if W == 1
-            make_zero(source; runtime_inactive=Val(false))
-        else
-            ntuple(Val(W)) do _
-                @inline
-                make_zero(source; runtime_inactive=Val(false))
-            end
-        end
+        make_zero(source; runtime_inactive)
     end
 
-    tape1 = if f isa Const
+    tape_segbuf = if !reverse_needs_segbuf
         nothing
     else
         @assert !isnothing(eval_segbuf_new)  # sanity check, should compile away
         eval_segbuf_new
     end
-    tape2 = (RT <: Active) ? nothing : shadow
-    tape = (tape1, tape2)
+    tape_fval = if EnzymeRules.overwritten(config)[2]
+        # wrap in VectorSpace before deepcopy to utilize a specialization that calls
+        # recursive_map, which is more performant than generic deepcopy
+        deepcopy(VectorSpace(fv; runtime_inactive)).val
+    else
+        nothing
+    end
+    tape = (tape_segbuf, shadow, tape_fval)
 
-    return AugmentedReturn(primal, shadow, tape)
+    return EnzymeRules.AugmentedReturn(primal, shadow, tape)
 end
 
+# function EnzymeRules.reverse(
+#     ::EnzymeRules.RevConfigWidth{1},
+#     ::Const{typeof(do_quadgk)},
+#     ::Type{<:Const},
+#     tape,
+#     f::Annotation,
+#     segs::Union{Const{S},Active{S}},
+#     n::Const,
+#     atol::Union{Const,Active},  # never actually active, but may be inferred as such by runtime activity
+#     rtol::Union{Const,Active},  # never actually active, but may be inferred as such by runtime activity
+#     maxevals::Const,
+#     nrm::Const,
+#     segbuf::Const,
+#     eval_segbuf::Const,
+# ) where {N,T,S<:NTuple{N,T}}
+#     df = if f isa Active
+#         make_zero(f.val; runtime_inactve)
+#     else
+#         if !(f isa Const)
+#             make_zero!(f.dval; runtime_inactive)
+#         end
+#         nothing
+#     end
+#     dsegs = (segs isa Const) ? nothing : make_zero(segs.val; runtime_inactive)
+#     datol = (atol isa Const) ? nothing : make_zero(atol.val; runtime_inactive)
+#     drtol = (rtol isa Const) ? nothing : make_zero(rtol.val; runtime_inactive)
+#     return (df, dsegs, nothing, datol, drtol, nothing, nothing, nothing, nothing)
+# end
+
 function EnzymeRules.reverse(
-    ::RevConfig,
+    config::EnzymeRules.RevConfigWidth{1},
     ::Const{typeof(do_quadgk)},
     shadow::Active,
-    tape,
-    f::Annotation,
-    segs::Union{Const{NTuple{N,T}},Active{NTuple{N,T}}},
+    tape::Tuple{Any,Nothing,Any},
+    f::F,
+    segs::Union{Const{S},Active{S}},
     n::Const,
     atol::Union{Const,Active},  # never actually active, but may be inferred as such by runtime activity
     rtol::Union{Const,Active},  # never actually active, but may be inferred as such by runtime activity
@@ -100,41 +129,58 @@ function EnzymeRules.reverse(
     nrm::Const,
     segbuf::Const,
     eval_segbuf::Const,
-) where {N,T}
+) where {F,N,T,S<:NTuple{N,T}}
     dI = first(shadow.val)
-    mode = ReverseSplitNoPrimal
-    fwd, rev = autodiff_thunk(mode, Const{typeof(call)}, Active, typeof(f), Const{T})
-    segsv, nv = (segs.val, n.val)
+    eval_segbuf_r = first(tape)
+    # XXX: workaround for Enzyme.jl#2304: would like to use the primal from the tape and
+    # accumulate into the shadow from the argument, but they often contain values with
+    # mismatched shapes, so this may break
+    ff = EnzymeRules.overwritten(config)[2] ? mergeannotation(last(tape), f) : f
+    fwd, rev = autodiff_thunk(ReverseSplitNoPrimal, Const{typeof(call)}, Active, F, Const{T})
+    segsv, nv = segs.val, n.val
     atol_r = nothing  # don't use primal atol, it's dimensionally incorrect for gradient
     segbuf_r = nothing
-    eval_segbuf_r = first(tape)
-    # TODO: Need to decide about accurate and adaptive vs. discretized and fast gradient. Use preferences and let the user decide?
+    # TODO: need to decide about accurate and adaptive vs. discretized and fast gradient.
+    #
     # A: accurate
     # rtol_r = rtol.val
     # maxevals_r = maxevals.val
     # nrm_r = nrm.val
+    #
     # B: fast
     rtol_r = nothing
     maxevals_r = 0
     zI = zero(real(dI))
     nrm_r(@nospecialize(x)) = zI
+    #
+    # TODO: use preferences and let the user decide?
     df = reverse_inner!!(
-        f, fwd, rev, dI, segsv, nv, atol_r, rtol_r, maxevals_r, nrm_r, segbuf_r, eval_segbuf_r
+        ff, fwd, rev, dI, segsv, nv, atol_r, rtol_r, maxevals_r, nrm_r, segbuf_r, eval_segbuf_r
     )
     dsegs = if segs isa Const
         nothing
     else
-        # XXX: This evaluates f at endpoints, which quadgk promises not to do
+        # XXX: this evaluates f at endpoints, which quadgk promises not to do---but only if
+        # you're differentiating wrt. endpoints, in which case the user asked for it, except
+        # they may not have; constant endpoints may be passed as active due to Enzyme
+        # activity inference limitations rather than user intent
+        fv = ff.val
         seg1, segN = segsv[1], segsv[N]
-        dseg1 = -LinearAlgebra.dot(f.val(seg1), dI)
-        dsegN = LinearAlgebra.dot(f.val(segN), dI)
-        zdseg = zero(dseg1)
-        (dseg1, ntuple(_ -> zdseg, Val(N - 2))..., dsegN)
+        dseg1 = -LinearAlgebra.dot(fv(seg1), dI)
+        dsegN = LinearAlgebra.dot(fv(segN), dI)
+        dseg_zero = zero(dseg1)
+        (dseg1, ntuple(_ -> dseg_zero, Val(N - 2))..., dsegN)
     end
-    datol = (atol isa Const) ? nothing : zero(dI / atol.val)
-    drtol = (rtol isa Const) ? nothing : zero(dI / rtol.val)
+    datol = (atol isa Const) ? nothing : make_zero(atol.val; runtime_inactive)
+    drtol = (rtol isa Const) ? nothing : make_zero(rtol.val; runtime_inactive)
     return (df, dsegs, nothing, datol, drtol, nothing, nothing, nothing, nothing)
 end
+
+mergeannotation(newxval::T, ::Const{T}) where {T} = Const(newxval)::Const{T}
+mergeannotation(newxval::T, ::Active{T}) where {T} = Active(newxval)::Active{T}
+mergeannotation(newxval::T, x::Duplicated{T}) where {T} = Duplicated(newxval, x.dval)::Duplicated{T}
+mergeannotation(newxval::T, x::DuplicatedNoNeed{T}) where {T} = DuplicatedNoNeed(newxval, x.dval)::DuplicatedNoNeed{T}
+mergeannotation(newxval::T, x::MixedDuplicated{T}) where {T} = MixedDuplicated(newxval, x.dval)::MixedDuplicated{T}
 
 call(f::F, x::T) where {F,T} = f(x)
 
@@ -143,52 +189,48 @@ reverse_inner!!(::Const, @nospecialize(args...)) = nothing
 function reverse_inner!!(f::Active, fwd, rev, dI, segs, args::Vararg{Any,M}) where {M}
     df_VS, _ = do_quadgk(segs, args...) do x
         tape, _, _ = fwd(Const(call), f, Const(x))
-        dfx, _ = only(rev(Const(call), f, Const(x), dI, tape))
-        return VectorSpace(dfx; runtime_inactive=Val(false))
+        dfx = first(only(rev(Const(call), f, Const(x), dI, tape)))
+        return VectorSpace(dfx; runtime_inactive)
     end
     return df_VS.val
 end
 
 function reverse_inner!!(
-    (; val, dval)::Union{Duplicated,DuplicatedNoNeed,BatchDuplicated,BatchDuplicatedNoNeed},
-    fwd, rev, dI, segsv::NTuple{N,T}, args::Vararg{Any,M},
+    f::Union{Duplicated,DuplicatedNoNeed}, fwd, rev, dI, segs::NTuple{N,T}, args::Vararg{Any,M}
 ) where {N,T,M}
-    function integrand!(dfx_VS, x)
+    fval = f.val
+    df_VS = VectorSpace(f.dval; runtime_inactive)
+    d_df_VS = zero(df_VS)
+    integrand! = InplaceIntegrand(d_df_VS, d_df_VS / oneunit(T)) do dfx_VS, x
         dfx = dfx_VS.val
-        make_zero!(dfx; runtime_inactive=Val(false))
-        f = Duplicated(val, dfx)
-        tape, _, _ = fwd(Const(call), f, Const(x))
-        rev(Const(call), f, Const(x), dI, tape)
+        make_zero!(dfx; runtime_inactive)
+        fx = Duplicated(fval, dfx)
+        tape, _, _ = fwd(Const(call), fx, Const(x))
+        rev(Const(call), fx, Const(x), dI, tape)
         return nothing
     end
-    df_VS = VectorSpace(dval; runtime_inactive=Val(false))
-    d_df_VS = zero(df_VS)
-    integrand_x = d_df_VS / oneunit(T) # pre-allocate array of correct type for integrand evaluations
-    do_quadgk(InplaceIntegrand(integrand!, d_df_VS, integrand_x), segsv, args...)
+    do_quadgk(integrand!, segs, args...)
     df_VS .+= d_df_VS
     return nothing
 end
 
-function reverse_inner!!(
-    (; val, dval)::Union{MixedDuplicated,BatchMixedDuplicated},
-    fwd, rev, dI, segs, args::Vararg{Any,M},
-) where {M}
-    dfx = Ref(dval[])  # NOTE: dfx wouldn't be thread safe, but quadgk itself is single-threaded so it's OK
+function reverse_inner!!(f::MixedDuplicated, fwd, rev, dI, segs, args::Vararg{Any,M}) where {M}
+    # NOTE: shared dfx would lead to data races if do_quadgk were multithreaded, but it's not
+    fval = f.val
+    fx = MixedDuplicated(fval, Ref{typeof(fval)}())
     d_df_VS, _ = do_quadgk(segs, args...) do x
-        dfx[] = make_zero(dfx[]; runtime_inactive=Val(false))  # zero out-of-place to avoid return value aliasing
-        f = MixedDuplicated(val, dfx)
-        tape, _, _ = fwd(Const(call), f, Const(x))
-        rev(Const(call), f, Const(x), dI, tape)
-        return VectorSpace(dfx[]; runtime_inactive=Val(false))
+        dfx = fx.dval
+        dfx[] = make_zero(fx.val; runtime_inactive)
+        tape, _, _ = fwd(Const(call), fx, Const(x))
+        rev(Const(call), fx, Const(x), dI, tape)
+        return VectorSpace(dfx[]; runtime_inactive)
     end
-    # NOTE: We take care to update all the mutable parts of dval in-place instead of only
-    # updating the outer Ref through something like dval[] = VectorSpace(dval[]) + d_df_VS.
-    # That way, if a user holds their own reference to a part of the shadow, it will be
-    # updated like they may expect. (I suppose this is not an acutal API guarantee, but
-    # Hyrum's law...)
-    dfx[] = d_df_VS.val  # reduce, reuse, recycle
-    dval_VS = VectorSpace(dval; runtime_inactive=Val(false))
-    dval_VS .+= VectorSpace(dfx; runtime_inactive=Val(false))
+    # NOTE: need to update all the mutable parts of dval in-place, not just the outer Ref,
+    # so f.dval[] = (VectorSpace(f.dval[]) + d_df_VS).val would be incorrect
+    d_df = fx.dval  # reduce, reuse, recycle
+    d_df[] = d_df_VS.val
+    df_VS = VectorSpace(f.dval; runtime_inactive)
+    df_VS .+= VectorSpace(d_df; runtime_inactive)
     return nothing
 end
 
